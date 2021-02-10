4620ebcd399c1d7263841c7c861f91f898c4962e
SchoolCom <aadd306@naver.com>
2021-02-10 17:30:55 +0900

    Map

---
+++ Castaway/Assets/Resources/Scriptes/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;        // 따라다닐 타겟 오브젝트의 Transfor
+    private Transform tr;                // 카메라 자신의 Transform
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+    }
+
+    void LateUpdate()
+    {
+        tr.position = new Vector3(target.position.x - 0.5f, target.position.y+15f, target.position.z -15f);
+        tr.LookAt(target);
+    }
+}
---
+++ Castaway/Assets/Resources/Scriptes/Player.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody characterRigidbody;
+
+    public static float MoveSpeed = 6.0f;
+    public static float RunSpeed =10f;
+    public float ApplySpeed = MoveSpeed;
+    public float rotSpeed = 360f;
+
+
+    CharacterController pcController;
+    Vector3 direction;
+
+    Animator animator;
+
+    void Start()
+    {
+        characterRigidbody = GetComponent<Rigidbody>();
+        pcController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        CharacterControl_Slerp();
+        animator.SetFloat("Speed", pcController.velocity.magnitude);
+        Input_Animation();
+        TryRun();
+        //Debug.Log(pcController.velocity.magnitude);
+
+    }
+    private void CharacterControl_Slerp()
+    {
+            Vector3 direction = new Vector3(Input.GetAxis("Horizontal"),
+                0,
+                Input.GetAxis("Vertical"));
+
+            if (direction.sqrMagnitude > 0.01f)
+            {
+                Vector3 forward = Vector3.Slerp(transform.forward,
+                    direction,
+                    rotSpeed * Time.deltaTime /
+                    Vector3.Angle(transform.forward, direction));
+                transform.LookAt(transform.position + forward);
+            }
+            //direction.y -= jumpSpeed * Time.deltaTime;
+            pcController.Move(direction * ApplySpeed * Time.deltaTime + Physics.gravity);
+        }
+    private void Input_Animation()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            animator.SetTrigger("Fishing");
+        }
+    }
+
+    private void TryRun()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Run();
+        }
+        if(Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            RunCancle();
+        }
+    }
+
+    private void Run()
+    {
+        animator.SetBool("Runing", true);
+        ApplySpeed = RunSpeed;
+    }
+
+    private void RunCancle()
+    {
+        animator.SetBool("Runing", false);
+        ApplySpeed = MoveSpeed;
+    }
+}
