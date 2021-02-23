b7ad0e5d2df1ea262f20aa6e172990147c5fa97b
GeunWon <kk3355@naver.com>
2021-02-23 22:26:41 +0900

    bonfire interactino UI 추가

--- Castaway/Assets/Resources/Scriptes/UI/Shop/Bonfire.cs
+++ Castaway/Assets/Resources/Scriptes/UI/Shop/Bonfire.cs
@@ -8,19 +8,24 @@ public class Bonfire : MonoBehaviour
     public Player playerPosition;
     public GameObject cookUI;
     public GameObject inventoryUI;
+    public GameObject interactionUI;
 
     private bool bCookUI = false;
     private void Update()
     {
         if (Vector3.Distance(playerPosition.transform.position, this.transform.position)<3.5f)
         {
-            Debug.Log("test");
+            interactionUI.SetActive(true);
             if (Input.GetButtonDown("Interation"))
             {
                 bCookUI = !bCookUI;
                 cookUI.SetActive(bCookUI);
                 inventoryUI.SetActive(bCookUI);
             }
+        }
+        else
+        {
+            interactionUI.SetActive(false);
         }
         
     }
