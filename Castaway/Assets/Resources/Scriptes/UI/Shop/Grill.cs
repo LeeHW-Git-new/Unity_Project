c3030d2b6a960ac95128274d2d6f75f94707e66e
incheon_gw <a01057554609@gmail.com>
2021-02-24 14:47:54 +0900

    무기 넘버 수정

--- Castaway/Assets/Resources/Scriptes/UI/Shop/Grill.cs
+++ Castaway/Assets/Resources/Scriptes/UI/Shop/Grill.cs
@@ -12,12 +12,7 @@ public class Grill : MonoBehaviour
     public GameObject subSlotL;
     public GameObject subSlotR;
 
-
-    private void Update()
-    {
-        CookingTest();
-    }
-
+    private bool cookingCheck = false;
     private void SelectRecipe()
     {
         selectRecipe = EventSystem.current.currentSelectedGameObject.GetComponent<RecipeBtn>().recipe;
@@ -34,13 +29,15 @@ public class Grill : MonoBehaviour
         subSlotR.GetComponent<Item>().itemName = selectRecipe.food2.mainFood.GetComponent<Item>().itemName;
         subSlotR.GetComponent<Item>().no = selectRecipe.food2.mainFood.GetComponent<Item>().no;
 
+        FoodCheck();
     }
 
 
-    public int CookingTest()
+    private void FoodCheck()
     {
         int slotCnt = Inventory.Instance.AllSlot.Count;
-        int cookCnt = 0;
+        int foodCntL = 0;
+        int foodCntR = 0;
 
         for(int i = 0; i< slotCnt; i++)
         {
@@ -49,19 +46,42 @@ public class Grill : MonoBehaviour
             if (!slot.isSlots())
                 continue;
 
-            if(slot.ItemReturn().no == subSlotL.GetComponent<Item>().no ||
-                slot.ItemReturn().no == subSlotR.GetComponent<Item>().no)
-            {
-                cookCnt += 1;
-            }
-            // slot.ItemReturn().no == subSlotR.GetComponent<Item>().no)
 
+            if (slot.ItemReturn().no == subSlotL.GetComponent<Item>().no)
+            {
+                foodCntL += 1;
+            }
+
+            if(slot.ItemReturn().no == subSlotR.GetComponent<Item>().no)
+            {
+                foodCntR += 1;
+            }
+            
         }
-        Debug.Log(cookCnt);
-        return cookCnt;
+
+        if(foodCntL == 1 && foodCntR == 1)
+        {
+            cookingCheck = true;
+            subSlotL.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+            subSlotR.GetComponent<Image>().color = new Color(255, 255, 255, 255);
+        }
+        else
+        {
+            cookingCheck = false;
+            subSlotL.GetComponent<Image>().color = new Color(255, 255, 255, 100);
+            subSlotR.GetComponent<Image>().color = new Color(255, 255, 255, 100);
+        }
+
+        Debug.Log(cookingCheck);
+
     }
 
 
+    private void Cooking(bool cookingCheck)
+    {
 
+
+
+    }
 
 }
