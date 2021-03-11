e08641bfb81c807cb36dc37f00c6fb6a1d42bc89
GeunWon <kk3355@naver.com>
2021-03-11 17:54:59 +0900

    player name 반영

--- Castaway/Assets/Resources/Scriptes/SceneChange.cs
+++ Castaway/Assets/Resources/Scriptes/SceneChange.cs
@@ -3,16 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-public class TestSceneChange : MonoBehaviour
+public class SceneChange : MonoBehaviour
 {
     public Slider volumeSlider;
-    // Start is called before the first frame update
+    public InputField inputField;
+
     void Start()
     {
         SoundManager.Instance.ChangeBGM("MenuBGM");
     }
 
-    // Update is called once per frame
+ 
     void Update()
     {
         BgmVolume();
@@ -27,9 +28,9 @@ public class TestSceneChange : MonoBehaviour
 
     void ChangeScene()
     {
+        GameManager.Instance.playerName = inputField.text;
         LoadingManager.LoadScene("Forest");
     }
-
 
     void Quit()
     {
