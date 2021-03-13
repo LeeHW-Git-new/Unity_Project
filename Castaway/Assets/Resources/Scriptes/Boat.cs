35ed1c87f226982b56f38f63e63e841392a32043
incheon_gw <a01057554609@gmail.com>
2021-03-13 10:12:46 +0900

    BoatFix anim, particle 추가

--- Castaway/Assets/Resources/Scriptes/Boat.cs
+++ Castaway/Assets/Resources/Scriptes/Boat.cs
@@ -17,18 +17,18 @@ public class Boat : MonoBehaviour
    
     void Update()
     {
-      
-       if (Vector3.Distance(GameObject.Find("Player").transform.position, transform.position) < 10.0f
-               && Input.GetKeyDown(KeyCode.E))
-       {
-            GameObject.Find("Player").transform.GetChild(1).gameObject.SetActive(false);
-            GameObject.Find("Main Camera").GetComponent<CameraController>().camState = CameraController.CamMode.Boat;
-           agent.SetDestination(target.position);
-           fakePlayer.SetActive(true);
-       }
-       
 
-       if(Vector3.Distance(this.transform.position, target.transform.position)<= 100.0f
+        if (Vector3.Distance(GameObject.Find("Player").transform.position, transform.position) < 10.0f
+               && Input.GetKeyDown(KeyCode.E))
+        {
+            this.GetComponent<ParticleSystem>().Play();
+            GameObject.Find("Player").GetComponent<Animator>().SetTrigger("BoatFix");
+            Invoke("BoatFix", 5.0f);
+        }
+
+
+
+        if (Vector3.Distance(this.transform.position, target.transform.position)<= 100.0f
             && GameObject.Find("Main Camera").GetComponent<CameraController>().camState != CameraController.CamMode.GameOver)
         {
             GameObject.Find("Main Camera").GetComponent<CameraController>().camState = CameraController.CamMode.End;
@@ -36,6 +36,16 @@ public class Boat : MonoBehaviour
 
     }
 
+
+    void BoatFix()
+    {
+        this.GetComponent<ParticleSystem>().Stop();
+        this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionY;
+        GameObject.Find("Player").transform.GetChild(1).gameObject.SetActive(false);
+        GameObject.Find("Main Camera").GetComponent<CameraController>().camState = CameraController.CamMode.Boat;
+        agent.SetDestination(target.position);
+        fakePlayer.SetActive(true);
+    }
 
 
 }
